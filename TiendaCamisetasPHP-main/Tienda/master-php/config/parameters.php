78f890a68233f1bd16f58aebbbbc4b1027617842
ivancer2002 <91883695+ivancer2002@users.noreply.github.com>
2022-02-20 20:41:04 +0100

    Camisetas PHP
    Terminado

---
+++ TiendaCamisetasPHP-main/Tienda/master-php/config/parameters.php
@@ -0,0 +1,5 @@
+<?php
+define("base_url", "http://localhost/taller/TiendaCamisetasPHP-main/Tienda/master-php/");
+define("controller_default", "productoController");
+define("action_default", "index");
+
